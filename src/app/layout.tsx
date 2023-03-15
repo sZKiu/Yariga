cce23701731ffff2c442314e2847928e5ff0c09f
sZKiu <augustomendez544@gmail.com>
2023-03-15 19:25:23 -0300

    seeing what is happening with refreshToken

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,40 +5,40 @@ import AlertDelAcc from "../components/common/AlertDeleteAcc";
 import { cookies } from "next/headers";
 import { UserwPassword } from "@/interfaces/interfaces";
 
-async function getMeInfo() {
-  const cookieStore = cookies();
-  const refreshToken = cookieStore.get("refreshToken");
+// async function getMeInfo() {
+//   const cookieStore = cookies();
+//   const refreshToken = cookieStore.get("refreshToken");
 
-  const meInfo = await fetch(
-    "https://apiexpressuser-3-k8787246.deta.app/api/v1/auth/me",
-    {
-      credentials: "include",
-      headers: {
-        "content-type": "application/json",
-        refreshtoken: refreshToken ? refreshToken.value : "",
-      },
-      cache: "no-store",
-    }
-  );
+//   const meInfo = await fetch(
+//     "https://apiexpressuser-3-k8787246.deta.app/api/v1/auth/me",
+//     {
+//       credentials: "include",
+//       headers: {
+//         "content-type": "application/json",
+//         refreshtoken: refreshToken ? refreshToken.value : "",
+//       },
+//       cache: "no-store",
+//     }
+//   );
 
-  return await meInfo.json();
-}
+//   return await meInfo.json();
+// }
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const meInfo: UserwPassword = await getMeInfo();
+  // const meInfo: UserwPassword = await getMeInfo();
 
   return (
     <html lang="en">
       <body className="font-[Roboto]">
         {/* @ts-ignore */}
-        <Header meInfo={meInfo} />
+        {/* <Header meInfo={meInfo} /> */}
 
         <main className="flex">
-          <Navigation showAll={Boolean(meInfo)} />
+          {/* <Navigation showAll={Boolean(meInfo)} /> */}
 
           {children}
         </main>
